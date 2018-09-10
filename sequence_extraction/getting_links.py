bb13ea73346899d7c4c8f4f80916c48e46e72416
gowthamv441 <31705808+gowthamv441@users.noreply.github.com>
2018-09-10 12:24:45 -0700

    under process

--- sequence_extraction/getting_links.py
+++ sequence_extraction/getting_links.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 if __name__=='__main__':
-    url = raw_input("Enter a website to extract the URL's from: ")
+    url = "www.ncbi.nlm.nih.gov/gene/5792"
     r  = requests.get("http://" +url)
     data = r.text
     soup = BeautifulSoup(data)
+    pages=[]
     for link in soup.find_all('a'):
-        print(link.get('href'))
+        page=link.get('href')
+        #if page.find("term")!=-1:
+        pages.append(page)
+    for page in pages:
+        if page=="/nuccore/NC_000001.11?report=fasta&from=43522238&to=43623672":
+            print (url+page)
