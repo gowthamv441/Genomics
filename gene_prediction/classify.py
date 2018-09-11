c7084ed61cb303c12ade94dda99bbd2e33d10fe2
gowthamv441 <31705808+gowthamv441@users.noreply.github.com>
2018-09-11 11:28:02 -0700

    inference time updated

--- gene_prediction/classify.py
+++ gene_prediction/classify.py
@@ -11,6 +11,7 @@ import pickle
 import cv2
 import os
 import glob
+import time
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -27,40 +28,43 @@ args = vars(ap.parse_args())
 img_list=glob.glob(args["image"]+"/*.png")
 img_count=1
 for item in img_list:
-    image = cv2.imread(item)
-    output = imutils.resize(image, width=400)
+	image = cv2.imread(item)
+	output = imutils.resize(image, width=400)
 
-    # pre-process the image for classification
-    image = cv2.resize(image, (96, 96))
-    image = image.astype("float") / 255.0
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
+	# pre-process the image for classification
+	image = cv2.resize(image, (96, 96))
+	image = image.astype("float") / 255.0
+	image = img_to_array(image)
+	image = np.expand_dims(image, axis=0)
 
-    # load the trained convolutional neural network and the multi-label
-    # binarizer
-    print("[INFO] loading network...")
-    model = load_model(args["model"])
-    mlb = pickle.loads(open(args["labelbin"], "rb").read())
+	# load the trained convolutional neural network and the multi-label
+	# binarizer
+	print("[INFO] loading network...")
+	model = load_model(args["model"])
+	mlb = pickle.loads(open(args["labelbin"], "rb").read())
 
-    # classify the input image then find the indexes of the two class
-    # labels with the *largest* probability
-    print("[INFO] classifying image...")
-    proba = model.predict(image)[0]
-    idxs = np.argsort(proba)[::-1][:2]
+	# classify the input image then find the indexes of the two class
+	# labels with the *largest* probability
+	print("[INFO] classifying image...")
+	st=time.time()
+	proba = model.predict(image)[0]
+	done=time.time()
+	idxs = np.argsort(proba)[::-1][:2]
+	print ("Inference Time :: {}".format(done-st))
 
-    # loop over the indexes of the high confidence class labels
-    for (i, j) in enumerate(idxs):
-    	# build the label and draw the label on the image
-    	label = "{}: {:.2f}%".format(mlb.classes_[j], proba[j] * 100)
-    	cv2.putText(output, label, (10, (i * 30) + 25),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+	# loop over the indexes of the high confidence class labels
+	for (i, j) in enumerate(idxs):
+		# build the label and draw the label on the image
+		label = "{}: {:.2f}%".format(mlb.classes_[j], proba[j] * 100)
+		cv2.putText(output, label, (10, (i * 30) + 25),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-    # show the probabilities for each of the individual labels
-    for (label, p) in zip(mlb.classes_, proba):
-    	print("{}: {:.2f}%".format(label, p * 100))
+	# show the probabilities for each of the individual labels
+	for (label, p) in zip(mlb.classes_, proba):
+		print("{}: {:.2f}%".format(label, p * 100))
 
-    # show the output image
-    cv2.imwrite("./outputs/test_"+str(img_count)+".png", output)
-    img_count+=1
-    print ("[INFO] Successfully predicted {}".format(item))
-    print ("====================================================")
+	# show the output image
+	cv2.imwrite("./outputs/test_"+str(img_count)+".png", output)
+	img_count+=1
+	print ("[INFO] Successfully predicted {}".format(item))
+	print ("====================================================")
