b12e66e3576aeb13f684a283fd6907093a363894
gowthamv441 <31705808+gowthamv441@users.noreply.github.com>
2018-09-19 01:08:40 -0700

    graphical approach

---
+++ graphical_representation/sample.py
@@ -0,0 +1,17 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+G.add_edges_from(
+    [('A', 'B'), ('A', 'C'), ('D', 'B'), ('E', 'C'), ('E', 'F'),
+     ('B', 'H'), ('B', 'G'), ('B', 'F'), ('C', 'G')])
+
+val_map = {'A': 1.0,
+           'D': 0.5714285714285714,
+           'H': 0.0}
+
+values = [val_map.get(node, 0.25) for node in G.nodes()]
+
+nx.draw(G, cmap = plt.get_cmap('jet'), node_color = values)
+plt.show()
